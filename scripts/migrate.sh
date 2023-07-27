f5f1ec900d625290f0f7345a2dda811024059f1c
dczysz <32844299+dczysz@users.noreply.github.com>
2023-07-27 11:58:57 -0600

    push instead of migrate?

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,4 +6,5 @@ mkswap /swapfile
 echo 10 >/proc/sys/vm/swappiness
 swapon /swapfile
 echo 1 >/proc/sys/vm/overcommit_memory
-npx prisma migrate deploy
+# npx prisma migrate deploy
+npx prisma db push
