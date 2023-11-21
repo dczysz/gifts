1a3fa56d4d815deb3d5738c9fe841d2628d6a915
dczysz <32844299+dczysz@users.noreply.github.com>
2023-11-21 15:18:34 -0700

    add basic favicon

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,27 @@ export const links: LinksFunction = () => {
       href: globalLargeStylesUrl,
       media: "screen and (min-width: 1024px)",
     },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
   ];
 };
 
